f1849b20eb973abed563be8adf838afbb2465f68
caffix <caffix@users.noreply.github.com>
2024-09-21 00:23:18 -0400

    change the color of a Fingerprint asset again

--- viz/d3.go
+++ viz/d3.go
@@ -321,7 +321,7 @@ func WriteD3Data(output io.Writer, nodes []Node, edges []Edge) error {
 		string(oam.EmailAddress):     "chocolate",
 		string(oam.Location):         "darkgray",
 		string(oam.Phone):            "coral",
-		string(oam.Fingerprint):      "chartreuse1",
+		string(oam.Fingerprint):      "darkgoldenrod",
 		string(oam.Organization):     "aqua",
 		string(oam.Person):           "bisque",
 		string(oam.TLSCertificate):   "deeppink",
